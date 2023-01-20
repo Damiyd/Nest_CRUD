7d3927cd603ff2f8ad3d234795d25686e6645e34
Damiyd <dpekfa144@gmail.com>
2023-01-20 20:48:56 +0900

    feat: Interceptor 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,21 @@
-import { Controller, Delete, Get, HttpException, Param, Post, Put, UseFilters } from '@nestjs/common';
-import { HttpExceptionFilter } from 'src/http-exception.filter';
+import { Controller, Delete, Get, HttpException, Param, ParseIntPipe, Post, Put, UseFilters, UseInterceptors } from '@nestjs/common';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptors';
+import { HttpExceptionFilter } from '../common/exceptions/http-exception.filter';
 import { UsersService } from './users.service';
 
 @Controller('users')
+@UseInterceptors(SuccessInterceptor)
 @UseFilters(HttpExceptionFilter)
 export class UsersController {
     constructor (private readonly usersService : UsersService) {}
 
     @Get()
     allAllUser() {
-        throw new HttpException("api is broken", 401)
         return "all user";
     }
 
     @Get(":id")
-    getOneUser(@Param() param) {
+    getOneUser(@Param("id", ParseIntPipe) param: number) {
         return "one user";
     }
 
